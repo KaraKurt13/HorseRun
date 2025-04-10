3001d223dc588768d97670bafc98fa45a61da858
KaraKurt <penguin.ners@gmail.com>
2025-04-10 17:05:51 +0300

    Basic ui components

---
+++ Assets/Scripts/UI/GameResultsComponent.cs
@@ -0,0 +1,25 @@
+using Assets.Scripts.UI.Infrastructure;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class GameResultsComponent : ComponentBase
+    {
+        public override void Draw()
+        {
+            base.Draw();
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+        }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Infrastructure/ComponentBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Infrastructure
+{
+    public abstract class ComponentBase : MonoBehaviour
+    {
+        public virtual void Draw()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public virtual void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public virtual void Refresh() { }
+    }
+}
